7b4518888d73a1dbb1de5033af9de4011bb6a77a
hyunho <mizzking75@gmail.com>
2023-07-04 00:36:28 +0900

    Initial Commit

---
+++ Jujob/JujobApp.swift
@@ -0,0 +1,17 @@
+//
+//  JujobApp.swift
+//  Jujob
+//
+//  Created by hyunho lee on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct JujobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
